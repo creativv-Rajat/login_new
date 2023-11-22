991d4c415428f97a6ab503065baccac4464e0161
creativv-Rajat <143613355+creativv-Rajat@users.noreply.github.com>
2023-11-22 08:26:05 +0400

    Add files via upload

---
+++ ggh.c
@@ -0,0 +1,88 @@
+/*  data-type="icon"
+        data-shape="circle"
+        data-theme="outline"
+        data-text="signin_with"
+               <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-image: url('marsi.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            height: 90vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size:30px;
+        }
+
+        .container {
+            width: 500px;
+            height:500px;
+            background-color: rgba(255, 0, 0, 0.2);
+            padding: 30px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        form {
+            margin-bottom: 1px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+        }
+
+        button {
+            background-color: rgb(88, 18, 77);
+            color: white;
+            padding: 10px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+    </style>*/
+    /*@import url("https://fonts.googleapis.com/css?family=Ceviche+One");
+.center_div{
+    width:100vw;
+    height:100vh;
+    display:grid;
+    place-items:center;
+    }
+    button:hover {
+        cursor: pointer;
+      }
+      body {
+    background-image: url("mars.jpg");
+    background-size:     cover;                     
+    background-repeat:   no-repeat;
+    background-position: center ; 
+    }
+    
+    label {
+        font-family: "Ceviche One";
+        font-size: 30px;
+        color: white;
+        font-weight: 500;
+        text-align: center;
+      }    
+    .style{margin:0;
+        padding:0;
+        box-sizing:border-box;
+        font-family:"Ceviche One";
+    color: aliceblue;}*/
